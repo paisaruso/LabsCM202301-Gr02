88dc0777ebc68a88c83b152ed37458ad4171e516
paisaruso <enrique.oregoudea.edu.co>
2023-03-08 21:28:32 -0500

    clase activity_personal_data instanciada

---
+++ Lab1-UI/src/main/java/exoesqueletor/co/edu/udea/compumovil/gr02_202301/lab1gr02/ActividadPersonalDatos.kt
@@ -0,0 +1,13 @@
+package exoesqueletor.co.edu.udea.compumovil.gr02_202301.lab1gr02
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ActividadPersonalDatos : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_personal_data)
+        }
+    }
+
--- Lab1-UI/src/main/java/exoesqueletor/co/edu/udea/compumovil/gr02_202301/lab1gr02/MainActivity.kt
+++ Lab1-UI/src/main/java/exoesqueletor/co/edu/udea/compumovil/gr02_202301/lab1gr02/MainActivity.kt
@@ -1,11 +1,23 @@
 package exoesqueletor.co.edu.udea.compumovil.gr02_202301.lab1gr02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+private lateinit var btnDataPersonVari: Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnDataPersonVari = findViewById(R.id.btnDataPerson)
+
+        btnDataPersonVari.setOnClickListener {
+            val intent = Intent(this, ActividadPersonalDatos::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
