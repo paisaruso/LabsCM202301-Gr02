f9fd75ba70d4b9826966e8d654dfd19b93564633
paisaruso <enrique.oregoudea.edu.co>
2023-03-12 19:16:34 -0500

    los campos de la activity PersonalDataActivity validados todos

--- Lab1-UI/src/main/java/exoesqueletor/co/edu/udea/compumovil/gr02_202301/lab1gr02/ActividadPersonalDatos.kt
+++ Lab1-UI/src/main/java/exoesqueletor/co/edu/udea/compumovil/gr02_202301/lab1gr02/ActividadPersonalDatos.kt
@@ -2,9 +2,7 @@ package exoesqueletor.co.edu.udea.compumovil.gr02_202301.lab1gr02
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.text.DateFormat
 import java.util.*
@@ -14,6 +12,12 @@ class ActividadPersonalDatos : AppCompatActivity(), DatePickerDialog.OnDateSetLi
     private lateinit var btnCambiar: Button
     private lateinit var tvFechaNacimiento: TextView
     private val calendar: Calendar = Calendar.getInstance()
+    private lateinit var spnEscolaridad: Spinner
+    private lateinit var btnSiguiente: Button
+    private lateinit var edtNombres: EditText
+    private lateinit var edtApellidos: EditText
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -21,11 +25,40 @@ class ActividadPersonalDatos : AppCompatActivity(), DatePickerDialog.OnDateSetLi
 
         btnCambiar = findViewById(R.id.btn_cambiar)
         tvFechaNacimiento = findViewById(R.id.tv_fechaNacimiento)
+        spnEscolaridad = findViewById(R.id.spn_escolaridad)
+        btnSiguiente = findViewById(R.id.btn_siguiente)
+        edtNombres = findViewById<EditText>(R.id.edt_nombres)
+        edtApellidos = findViewById<EditText>(R.id.edt_apellidos)
+
 
         btnCambiar.setOnClickListener {
             showDatePickerDialog()
         }
-    }
+        // Definir la función de clic
+        btnSiguiente.setOnClickListener {
+            validarSpinner(spnEscolaridad, edtNombres, edtApellidos, tvFechaNacimiento)
+        }
+        }
+        private fun validarSpinner(spinner: Spinner, adtNombres: EditText, adtApellidos:EditText, tvFechaNacimiento: TextView) {
+            val seleccion = spinner.selectedItem.toString()
+            val nombre = edtNombres.text.toString().trim()
+            val apellido = edtApellidos.text.toString().trim()
+            val FechaNacimiento = tvFechaNacimiento.text.toString().trim()
+
+            if (seleccion.isEmpty()) {
+                Toast.makeText(this, "Debe seleccionar un valor en el spinner", Toast.LENGTH_SHORT).show()
+            } else if (nombre.isEmpty()) {
+                Toast.makeText(this, "Debe ingresar un nombre en el campo", Toast.LENGTH_SHORT).show()
+            }else if(apellido.isEmpty()){
+                Toast.makeText(this, "Debe ingresar un apellido en el campo", Toast.LENGTH_SHORT).show()
+            }else if(FechaNacimiento.isEmpty()){
+                Toast.makeText(this, "Debe ingresar una fecha en el campo", Toast.LENGTH_SHORT).show()
+            }else{
+                //llamremos la tercera activity
+                Toast.makeText(this, "4 campos validados", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     private fun showDatePickerDialog() {
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
@@ -40,5 +73,6 @@ class ActividadPersonalDatos : AppCompatActivity(), DatePickerDialog.OnDateSetLi
         val currentDateString = DateFormat.getDateInstance(DateFormat.SHORT).format(calendar.time)
         tvFechaNacimiento.text = " $currentDateString"
     }
+
 }
 
